d206e9d8875a308cb8fae04d6277422f70917aa4
violetstair <violetstair@gmail.com>
2020-06-26 18:01:08 +0900

    Fix: permission check error without token

--- users/permissions.py
+++ users/permissions.py
@@ -4,13 +4,9 @@ from rest_framework import permissions
 class IsOwner(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        if request._auth['user_id'] == view.kwargs['pk']:
-            return True
-        else:
-            print('*' * 55)
-            print(request._auth['user_id'], ' == ', view.kwargs['pk'])
-            print('*' * 55)
-            return False
+        if request._auth != None and 'pk' in view.kwargs:
+            return request._auth['user_id'] == view.kwargs['pk']
+        return False
 
     def has_object_permission(self, request, view, obj):
         return obj == request.user
